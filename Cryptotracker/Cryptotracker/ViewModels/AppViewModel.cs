e2e14a309a9b02d0d087f4a6df21aba3b83309f9
Szymon Świstek <szymonswistek6@gmail.com>
2021-03-20 19:48:07 +0100

    dodano główny view model oraz pakiet FodyPropertyChanged w celu łatwiejszego binding'u zmiennych

---
+++ Cryptotracker/Cryptotracker/ViewModels/AppViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Cryptotracker.ViewModels
+{
+    public class AppViewModel : INotifyPropertyChanged
+    {
+        public Version AppVersion { get; } = Assembly.GetEntryAssembly().GetName().Version;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
