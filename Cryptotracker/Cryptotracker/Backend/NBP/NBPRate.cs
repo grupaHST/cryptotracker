c202a5bb48930a316bbf57230fe8fb9a55f1a0b2
Oswald Toma <oswald.toma@gmail.com>
2021-04-11 14:18:12 +0200

    Dodano klasy dla danych o kursach z NBP

---
+++ Cryptotracker/Cryptotracker/Backend/NBP/NBPRate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend
+{
+    public class NBPRate
+    {
+        public string No { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public double Mid { get; set; } //for table A and B
+
+        //for table C
+        public double Bid { get; set; } 
+        public double Ask { get; set; }
+    }
+}
