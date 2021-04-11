8310b206a882cb1266edc43174a6767d7d80bb8a
Oswald Toma <oswald.toma@gmail.com>
2021-04-11 14:20:02 +0200

    Dodano klasę brzegową zajmującą się deserializacją danych (na ten moment NBP bez ram czasowych) + przygotowana klasa do testów

---
+++ Cryptotracker/CryptotrackerTests/Backend/ExchangeRatesTest.cs
@@ -0,0 +1,23 @@
+﻿using Cryptotracker.Backend;
+using Cryptotracker.Backend.NBP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptotrackerTests.Backend
+{
+    [TestClass]
+    public class ExchangeRatesTest
+    {
+        [TestMethod]
+        public void GetCurrencyDataTest()
+        {
+            object result = ExchangeRates.GetCurrencyData(ExchangePlatform.NBP, CurrencyCode.CHF);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
