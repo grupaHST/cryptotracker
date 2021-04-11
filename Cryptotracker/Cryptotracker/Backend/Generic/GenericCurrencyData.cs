4edc027faf68819045c4eb945bc305ab4eab234f
Oswald Toma <oswald.toma@gmail.com>
2021-04-11 14:17:50 +0200

    Dodano klasy generyczne dla kursu danej waluty

---
+++ Cryptotracker/Cryptotracker/Backend/Generic/GenericCurrencyData.cs
@@ -0,0 +1,18 @@
+﻿using Cryptotracker.Backend.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend
+{
+    public class GenericCurrencyData
+    {
+        public string CurrencyName { get; set; }
+        public string Code { get; set; }
+        public List<GenericRate> Rates { get; set; }
+        public bool ErrorOccured { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ Cryptotracker/Cryptotracker/Backend/Generic/GenericRate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend.Generic
+{
+    public class GenericRate
+    {
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
