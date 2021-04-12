7d67b23ff41e8cb5b2a8e05c7d90d7bc76da087a
Szymon161 <szymonswistek6@gmail.com>
2021-04-12 20:19:16 +0200

    dodano strukturę z ustawieniami aplikacji wraz z testami jednostkowymi

---
+++ Cryptotracker/Cryptotracker/LocalData/LocalAppSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Cryptotracker.LocalData
+{
+    public struct LocalAppSettings : IEquatable<LocalAppSettings>
+    {
+        public string Language { get; set; }
+        public string BaseColorScheme { get; set; }
+        public string ColorScheme { get; set; }
+
+        public bool Equals(LocalAppSettings other)
+        {
+            var currentElement = this;
+
+            return typeof(LocalAppSettings).GetProperties().ToList()
+                .All(x => x.GetValue(currentElement) == x.GetValue(other));
+        }
+
+        public override bool Equals(object obj) => obj is LocalAppSettings settings && Equals(settings);
+        public static bool operator ==(LocalAppSettings left, LocalAppSettings right) => left.Equals(right);
+        public static bool operator !=(LocalAppSettings left, LocalAppSettings right) => !left.Equals(right);
+        public override int GetHashCode() => base.GetHashCode();
+    }
+}
---
+++ Cryptotracker/CryptotrackerTests/LocalData/LocalAppSettingsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cryptotracker.LocalData.Tests
+{
+    [TestClass]
+    public class LocalAppSettingsTests
+    {
+        [TestMethod]
+        public void EqualsTest()
+        {
+            LocalAppSettings firstStruct = new()
+            {
+                Language = "English",
+                BaseColorScheme = "Base",
+                ColorScheme = "Yellow",
+            };
+
+            LocalAppSettings secondStruct = firstStruct;
+
+            Assert.AreEqual(firstStruct, secondStruct);
+            Assert.IsTrue(firstStruct == secondStruct);
+
+            secondStruct.ColorScheme = "Green";
+            Assert.IsFalse(firstStruct == secondStruct);
+            Assert.IsTrue(firstStruct != secondStruct);
+        }
+    }
+}
