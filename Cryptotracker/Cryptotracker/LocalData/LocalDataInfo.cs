91c6fa4bbbcc82b4791d565b678208b3d387c6c8
Szymon161 <szymonswistek6@gmail.com>
2021-04-12 21:05:24 +0200

    dodano klasę LocalDataManager wczytującą i zapisującą aktualne ustawienia programu

---
+++ Cryptotracker/Cryptotracker/LocalData/LocalDataInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Cryptotracker.LocalData
+{
+    public static class LocalDataInfo
+    {
+        public readonly static string Directory = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}" +
+                                                  $"{Path.DirectorySeparatorChar}{Assembly.GetEntryAssembly().GetName().Name}" +
+                                                  $"{Path.DirectorySeparatorChar}{Assembly.GetEntryAssembly().GetName().Version}";
+    }
+}
