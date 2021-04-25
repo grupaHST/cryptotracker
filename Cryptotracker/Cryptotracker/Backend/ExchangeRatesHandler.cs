66d6b57a5f8e77d68e3635827696d312ce3adb2e
Oswald Toma <oswald.toma@gmail.com>
2021-04-25 11:09:08 +0200

    Przeróbki w NBP - zwraca tylko dostępne dni, jeśli zakres dni jest niedostępny to zwraca nulla. Przeróbka w Rates - zwraca tylko dostępne dni, wyeliminowano podkładanie danych z ostatniego dostępnego dnia, pod niedostępną datę

--- Cryptotracker/Cryptotracker/Backend/ExchangeRatesHandler.cs
+++ Cryptotracker/Cryptotracker/Backend/ExchangeRatesHandler.cs
@@ -30,76 +30,123 @@ namespace Cryptotracker.Backend
             {
                 case ExchangePlatform.NBP:
                 {
-                    bool secondTable = false;
-                    char table = 'a';
                     string currencyCodeStr = currencyCode.ToString().ToLower();
+                    string startTimeStr = string.Empty;
+                    string endTimeStr = string.Empty;
+
+                    //check if currency is available in table a
+                    char table = 'a';
+
+                    try
+                    {
+                        requestURI = $"{basicNBPAPIAddress}/rates/{table}/{currencyCodeStr}";
+                        var test = await client.GetStringAsync(requestURI);
+                    }
+                    catch (Exception e)
+                    {
+                        table = 'b';
+                    }
+
+                    if (startTime.HasValue)
+                        startTimeStr = startTime.Value.ToString("yyyy-MM-dd");
+
+                    if (endTime.HasValue)
+                        endTimeStr = endTime.Value.ToString("yyyy-MM-dd");
 
                     bool sameDates = startTime.HasValue && endTime.HasValue ? startTime.Value == endTime.Value : true;
 
-                    while (true)
-                    {
-                        requestURI = $"{basicNBPAPIAddress}/rates/{table}/{currencyCodeStr}";
+                    var nbpData = new NBPCurrencyData();
+                    var rates = new List<GenericRate>();
 
-                        if (startTime.HasValue && endTime.HasValue && !sameDates)
-                        {
-                            string startTimeStr = startTime.Value.ToString("yyyy-MM-dd");
-                            requestURI += $"/{startTimeStr}";
-                            string endTimeStr = endTime.Value.ToString("yyyy-MM-dd");
-                            requestURI += $"/{endTimeStr}";
-                        }
-                        else if (startTime.HasValue)
-                        {
-                            string startTimeStr = startTime.Value.ToString("yyyy-MM-dd");
-                            requestURI += $"/{startTimeStr}";
-                        }
-                        else if (endTime.HasValue && !startTime.HasValue)
-                        {
-                            (Application.Current as App).LogMessage("NBP: No date has been provided.");
-                            return null;
-                        }
+                    if (startTime.HasValue && endTime.HasValue && !sameDates)
+                    {
+                        requestURI = $"{basicNBPAPIAddress}/rates/{table}/{currencyCodeStr}/{startTimeStr}/{endTimeStr}";
 
                         try
                         {
                             result = await client.GetStringAsync(requestURI).ConfigureAwait(false);
-                            break;
+                            nbpData = JsonSerializer.Deserialize<NBPCurrencyData>(result, jsonOptions);
                         }
                         catch (Exception e)
                         {
-                            //second try?
-                            if (table == 'b')
-                            {
-                                (Application.Current as App).LogMessage("NBP: No table has demanded currency data.");
-                                (Application.Current as App).LogMessage(e.Message);
-                                return null;
-                            }
+                            //(Application.Current as App).LogMessage("NBP: No data for given days.");
+                            //(Application.Current as App).LogMessage(e.Message);
+                            return null;
+                        }
 
-                            table = 'b';
+                        foreach (var rate in nbpData.Rates)
+                        {
+                            rates.Add(new GenericRate() { Date = rate.EffectiveDate, Value = rate.Mid });
                         }
                     }
+                    else if (startTime.HasValue)
+                    {
+                        requestURI = $"{basicNBPAPIAddress}/rates/{table}/{currencyCodeStr}/{startTimeStr}";
 
-                    var nbpData = JsonSerializer.Deserialize<NBPCurrencyData>(result, jsonOptions);
+                        try
+                        {
+                            result = await client.GetStringAsync(requestURI).ConfigureAwait(false);
+                            nbpData = JsonSerializer.Deserialize<NBPCurrencyData>(result, jsonOptions);
+                        }
+                        catch (Exception e)
+                        {
+                            //(Application.Current as App).LogMessage("NBP: No data for given day.");
+                            //(Application.Current as App).LogMessage(e.Message);
+                            return null;
+                        }
 
-                    var rates = new List<GenericRate>();
-                    nbpData.Rates.ForEach(x => rates.Add(new GenericRate() { Date = x.EffectiveDate, Value = x.Mid }));
+                        rates.Add(new GenericRate() { Date = nbpData.Rates.First().EffectiveDate, Value = nbpData.Rates.First().Mid });
+                    }
+                    else if (endTime.HasValue && !startTime.HasValue)
+                    {
+                        //(Application.Current as App).LogMessage("NBP: No start date has been provided.");
+                        return null;
+                    }
+                    else
+                    {
+                        requestURI = $"{basicNBPAPIAddress}/rates/{table}/{currencyCodeStr}";
 
-                    var currencyData = new GenericCurrencyData()
+                        try
+                        {
+                            result = await client.GetStringAsync(requestURI);
+                            nbpData = JsonSerializer.Deserialize<NBPCurrencyData>(result, jsonOptions);
+
+                        }
+                        catch (Exception)
+                        {
+                            return null;
+                        }
+
+                        rates.Add(new GenericRate() { Date = nbpData.Rates.First().EffectiveDate, Value = nbpData.Rates.First().Mid });
+                    }
+
+                    return new GenericCurrencyData()
                     {
                         Code = nbpData.Code,
                         Rates = rates
                     };
-
-                    return currencyData;
                 }
 
                 case ExchangePlatform.RATES:
                 {
                     string currencyCodeStr = currencyCode.ToString().ToUpper();
                     string baseAndSymbols = $"?base={currencyCodeStr}&symbols=PLN";
+                    string startTimeStr = string.Empty;
+                    string endTimeStr = string.Empty;
+
+                    if (startTime.HasValue)
+                        startTimeStr = startTime.Value.ToString("yyyy-MM-dd");
+
+                    if (endTime.HasValue)
+                        endTimeStr = endTime.Value.ToString("yyyy-MM-dd");
+
 
                     var rates = new List<GenericRate>();
                     var ratesAPIData = new RatesAPICurrencyData();
 
-                    bool bothDatesProvided = startTime.HasValue && endTime.HasValue;
+                    bool sameDates = startTime.HasValue && endTime.HasValue ? startTime.Value == endTime.Value : true;
+
+                    bool bothDatesProvided = startTime.HasValue && endTime.HasValue && !sameDates;
                     bool startDateProvided = startTime.HasValue;
                     bool noDateProvided = !startDateProvided && !bothDatesProvided;
 
@@ -110,13 +157,17 @@ namespace Cryptotracker.Backend
                         {
                             for (int i = 0; i < Math.Abs(daysSpanCount); i++)
                             {
-                                requestURI = $"{basicRatesAPIAddress}/{startTime.Value.AddDays(i).ToString("yyyy-MM-dd")}{baseAndSymbols}";
+                                var startTimeOffset = startTime.Value.AddDays(i);
+                                requestURI = $"{basicRatesAPIAddress}/{startTimeOffset.ToString("yyyy-MM-dd")}{baseAndSymbols}";
 
-                                result = await client.GetStringAsync(requestURI).ConfigureAwait(false);
-                                if(result == null)
+                                try
                                 {
-                                    (Application.Current as App).LogMessage("RatesAPI: Data range unavailable. Fatal Error.");
-                                    return null;
+                                    result = await client.GetStringAsync(requestURI).ConfigureAwait(false);
+                                }
+                                catch (Exception e)
+                                {
+                                    //(Application.Current as App).LogMessage("RatesAPI: Data range unavailable. Fatal Error.");
+                                    return null;                                    
                                 }
 
                                 var data = JsonSerializer.Deserialize<RatesAPICurrencyData>(result, jsonOptions);
@@ -124,13 +175,20 @@ namespace Cryptotracker.Backend
                                 double value = 0;
                                 data.Rates.TryGetValue("PLN", out value);
 
-                                rates.Add(new GenericRate() { Date = data.Date, Value = value });
+                                bool realDataReceived = data.Date == startTimeOffset;
+                                if (realDataReceived)
+                                {
+                                    rates.Add(new GenericRate() { Date = data.Date, Value = value });
+                                }
+                                else
+                                {
+                                    //(Application.Current as App).LogMessage($"RatesAPI: Data for {startTimeOffset} unavailable.");
+                                }
                             }
                         }
                     }
                     else if (startDateProvided)
                     {
-                        string startTimeStr = startTime.Value.ToString("yyyy-MM-dd");
                         requestURI = $"{basicRatesAPIAddress}/{startTimeStr}{baseAndSymbols}";
 
                         result = await client.GetStringAsync(requestURI).ConfigureAwait(false);
@@ -166,13 +224,11 @@ namespace Cryptotracker.Backend
                         rates.Add(new GenericRate() { Date = ratesAPIData.Date, Value = value });
                     }
 
-                    var currencyData = new GenericCurrencyData()
+                    return new GenericCurrencyData()
                     {
                         Code = currencyCode.ToString(),
                         Rates = rates
                     };
-
-                    return currencyData;
                 }
                 break;
 
