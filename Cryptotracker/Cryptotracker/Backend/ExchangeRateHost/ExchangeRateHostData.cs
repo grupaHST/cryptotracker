64286a8e22e8c66dac83a425af749e93aa985391
Oswald Toma <oswald.toma@gmail.com>
2021-04-30 20:04:06 +0200

    Dodano klasy do deserializacji danych z exchangerate.host

---
+++ Cryptotracker/Cryptotracker/Backend/ExchangeRateHost/ExchangeRateHostData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend.ExchangeRateHost
+{
+    public class ExchangeRateHostData
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public List<ExchangeRateHostRate> Rates { get; set; }
+    }
+}
