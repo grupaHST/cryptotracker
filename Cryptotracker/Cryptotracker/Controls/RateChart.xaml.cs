c824db04a25e1e4fe59fa0dc9220ddfd30c25e7b
dhlover <79374727+blackrider024@users.noreply.github.com>
2021-05-04 15:27:40 +0200

    Dodanie kontrolki RateChart

---
+++ Cryptotracker/Cryptotracker/Controls/RateChart.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Cryptotracker.Backend.Generic;
+using Cryptotracker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cryptotracker.Controls
+{
+    public partial class RateChart : UserControl 
+    {
+        public RateChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
