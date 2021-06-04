984356907206f61999044b73808f0b15953d87c3
Oswald Toma <oswald.toma@gmail.com>
2021-06-04 19:07:51 +0200

    Dodano model dla powiadomień. Member currentValue uzupełniany jest w menadżerze.

---
+++ Cryptotracker/Cryptotracker/Models/NotificationModel.cs
@@ -0,0 +1,46 @@
+﻿using Cryptotracker.Backend;
+using Cryptotracker.Backend.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Models
+{
+    public class NotificationModel
+    {
+        public enum CurrencyTypeEnum
+        {
+            CRYPTO,
+            FIAT
+        }
+
+        public readonly CurrencyCode CurrencyCode;
+        public readonly CryptocurrencyCode CryptocurrencyCode;
+        public readonly ExchangePlatform ExchangePlatform;
+        public readonly CryptoExchangePlatform CryptoExchangePlatform;
+        public readonly double Threshold;
+        public readonly Comparison Comparison;
+        public readonly CurrencyTypeEnum CurrencyType;
+        public double CurrentValue { get; set; }
+
+        public NotificationModel(CurrencyCode currencyCode, ExchangePlatform exchangePlatform, double threshold, Comparison comparison)
+        {
+            CurrencyCode = currencyCode;
+            ExchangePlatform = exchangePlatform;
+            Threshold = threshold;
+            Comparison = comparison;
+            CurrencyType = CurrencyTypeEnum.FIAT;
+        }
+
+        public NotificationModel(CryptocurrencyCode cryptocurrencyCode, CryptoExchangePlatform cryptoExchangePlatform, double threshold, Comparison comparison)
+        {
+            CryptocurrencyCode = cryptocurrencyCode;
+            CryptoExchangePlatform = cryptoExchangePlatform;
+            Threshold = threshold;
+            Comparison = comparison;
+            CurrencyType = CurrencyTypeEnum.CRYPTO;
+        }
+    }
+}
