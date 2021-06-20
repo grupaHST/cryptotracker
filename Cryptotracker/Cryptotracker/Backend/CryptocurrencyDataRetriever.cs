19103c3e37a133c822411441a7c137becb551992
Oswald Toma <oswald.toma@gmail.com>
2021-06-20 16:18:40 +0200

    Stworzono klasy abstrakcyjne dla retrieverów

---
+++ Cryptotracker/Cryptotracker/Backend/CryptocurrencyDataRetriever.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend
+{
+    public abstract class CryptocurrencyDataRetriever
+    {
+        public abstract Task<CurrencyDataModel> GetCurrencyData(CryptocurrencyCode cryptocurrencyCode, CryptoInterval interval = CryptoInterval.ONE_DAY, DateTime? startTime = null, DateTime? endTime = null);
+        public abstract Task<double> GetCurrentPrice(CryptocurrencyCode cryptocurrencyCode);
+    }
+}
---
+++ Cryptotracker/Cryptotracker/Backend/CurrencyDataRetriever.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend
+{
+    public abstract class CurrencyDataRetriever
+    {
+        public abstract Task<CurrencyDataModel> GetCurrencyData(ExchangePlatform currencyPlatform, CurrencyCode currencyCode, DateTime? startTime = null, DateTime? endTime = null);
+    }
+}
