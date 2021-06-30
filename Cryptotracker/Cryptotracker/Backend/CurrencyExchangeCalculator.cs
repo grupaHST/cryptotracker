80a3e397cb7280b2da6afc76daf1a7556d478470
dhlover <79374727+blackrider024@users.noreply.github.com>
2021-06-30 22:43:58 +0200

    CurrencyExchangeCalculator - GetCurrentExchangeValue

---
+++ Cryptotracker/Cryptotracker/Backend/CurrencyExchangeCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptotracker.Backend
+{
+    public static class CurrencyExchangeCalculator
+    {
+        public static async Task<double?> GetCurrentExchangeValue(string currencyCode, string exchangePlatform)
+        {
+            if (currencyCode == "PLN")
+                return 1;
+            else
+                return await ExchangeRatesHandler.GetFIATCurrentPrice(Enum.Parse<ExchangePlatform>(exchangePlatform), Enum.Parse<CurrencyCode>(currencyCode));
+        }
+    }
+}
